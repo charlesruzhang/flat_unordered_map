0f2e33832a92bf969e8eaffa7f9ae1a17a52edd4
charlesruzhang <ruzhang2@illinois.edu>
2025-09-03 14:50:26 -0500

    Done with timing unordered_map vs flat_map

--- flat.cpp
+++ flat.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h>
+#include <x86intrin.h>
 //Main Assumption here is that we are not adding elements at run time to the map.
+static const int TCOUNT = 1e6;
+static const int MAXX = 1e6;
+static const int INVCOUNT = 1e4;
+static constexpr double CPU_FREQ_HZ = 3201000000.0; // CHANGE THIS TO THE REAL FREQUENCY.
+static constexpr double CYCLES_TO_NS = 1e9 / CPU_FREQ_HZ;
+static inline uint64_t ticks() { _mm_lfence(); auto t=__rdtsc(); _mm_lfence(); return t; }
 struct smallstring {
     uint64_t hash;
     uint64_t sz; 
@@ -27,7 +34,7 @@ struct smallstringHash {
 };
 struct smallstringEq {
     bool operator()(const smallstring& a, const smallstring& b) const noexcept {
-        return a.hash == b.hash;
+        return a.hash == b.hash && a.s == b.s;
     }
 };
 template <typename K, typename V, typename Hash = std::hash<K>, typename Eq = std::equal_to<K>>
@@ -41,36 +48,33 @@ struct flat_map {
         int bit_length = std::bit_width(N);
         memory.resize(1ll << bit_length);
         mask = (1ll << bit_length) - 1;
-        std::cout << mask << std::endl;
         for (auto [k, v] : elements) {
             uint64_t index = hasher(k) & mask;
             memory[index].push_back({k, v});
         }
     }
 
-    V find (K& key) {
+    V find (const K& key) {
         uint64_t index = hasher(key) & mask;
-        for (auto [k, v] : memory[index]) {
+        for (auto& [k, v] : memory[index]) {
             if (eqer(key, k)) {
                 return v;
             }
         }
-        // We can use std::optional here, this assumes that V has a default constructor.
+        // We can use std::optional here, but here we assume that V has a default constructor.
         return V();
     }
 };
 
-
-int test1() {
+std::pair<std::vector<std::pair<smallstring, int>>, std::vector<smallstring>> gen_test() {
     std::vector<std::pair<smallstring, int>> elements;
     std::set<uint64_t> s;
     std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-    size_t MAXX = 1e6;
     int cnt = 0;
-    // We keep offset number of elements as non-existing keys;
-    int offset = 100;
+    // We have MAXX count of valid keys.
+    // We keep INVCOUNT number of elements as non-existing keys.
     std::vector<smallstring> bad_keys;
-    while (s.size() < MAXX + offset) {
+    while (s.size() < MAXX + INVCOUNT) {
         int len = std::uniform_int_distribution<int>(1, 8)(rng);
         std::string st;
         for (int i = 0; i < len; i++) {
@@ -87,36 +91,149 @@ int test1() {
             s.insert(sm.hash);
         }
     }
-    std::cout << "Test Start!" << std::endl;
+    return {elements, bad_keys};
+}
+int test_correctness(std::vector<std::pair<smallstring, int>>& elements, std::vector<smallstring>& bad_keys) {
+    std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+    std::cout << "Test Correctness Start!" << std::endl;
     flat_map<smallstring, int, smallstringHash, smallstringEq> fmap(elements);
     bool fail = 0;
-    for (int i = 0; i < MAXX; i++) {
+    std::vector<int> valid_tests(TCOUNT);
+    std::vector<int> invalid_tests(INVCOUNT);
+    for (int i = 0; i < TCOUNT; i++) {
         int index = std::uniform_int_distribution<int>(0, MAXX - 1)(rng);
-        int res = fmap.find(elements[index].first);
-        if (res != elements[index].second) {
+        valid_tests[i] = index;
+    }
+    
+    for (int i = 0; i < INVCOUNT; i++) {
+        int index = std::uniform_int_distribution<int>(0, INVCOUNT - 1)(rng);
+        invalid_tests[i] = index;
+    }
+
+    for (int i = 0; i < TCOUNT; i++) {
+        std::string s = elements[valid_tests[i]].first.s;
+        smallstring sm(s);
+        int res = fmap.find(sm);
+        if (res != elements[valid_tests[i]].second) {
             fail = 1;
             break;
         }
     }
-    for (int i = 0; i < offset; i++) {
-        int index = std::uniform_int_distribution<int>(0, offset - 1)(rng);
-        int res = fmap.find(bad_keys[index]);
+    
+    for (int i = 0; i < INVCOUNT; i++) {
+        std::string s = bad_keys[invalid_tests[i]].s;
+        smallstring sm(s);
+        int res = fmap.find(sm);
         if (res != 0) {
             fail = 1;
             break;
         }
     }
     if (fail) {
-        std::cout << "Test Failed :(" << std::endl;
+        std::cout << "Test Correctness Failed :(" << std::endl;
+        return 1;
     } else {
-        std::cout << "Test Passed :D" << std::endl;
+        std::cout << "Test Correctness Passed :D" << std::endl;
     }
-    // TODO: timer, readme.
-    //std::unordered_map<smallstring, int, smallstringHash> umap;
+    return 0;
+}
+void time_it (std::vector<uint64_t>& times) {
+    std::sort(times.begin(), times.end());
+    int TOTAL = times.size();
+    int p50 = TOTAL / 100 * 50 - 1;
+    double p50Time = times[p50] * CYCLES_TO_NS;
+    int p75 = TOTAL / 100 * 75 - 1;
+    double p75Time = times[p75] * CYCLES_TO_NS;
+    int p99 = TOTAL / 100 * 99 - 1;
+    double p99Time = times[p99] * CYCLES_TO_NS;
+    double aveTime =  (double) std::accumulate(times.begin(), times.end(), (uint64_t) 0) / TOTAL * CYCLES_TO_NS;
+    std::cout << "p50 Time = " << p50Time << "ns , p75 Time = " << p75Time << "ns, p99 Time = " << p99Time << "ns. " << std::endl;
+    std::cout << "Average Time = " << aveTime << "ns." << std::endl;
+}
+
+int test_time(std::vector<std::pair<smallstring, int>>& elements, std::vector<smallstring>& bad_keys) {
+    std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+    std::cout << "Test Start!" << std::endl;
+    flat_map<smallstring, int, smallstringHash, smallstringEq> fmap(elements);
+    bool fail = 0;
+    std::vector<int> valid_tests(TCOUNT);
+    std::vector<int> invalid_tests(INVCOUNT);
+    for (int i = 0; i < TCOUNT; i++) {
+        int index = std::uniform_int_distribution<int>(0, MAXX - 1)(rng);
+        valid_tests[i] = index;
+    }
+    for (int i = 0; i < INVCOUNT; i++) {
+        int index = std::uniform_int_distribution<int>(0, INVCOUNT - 1)(rng);
+        invalid_tests[i] = index;
+    }
+    std::vector<uint64_t> times_fmap_valid(TCOUNT);
+    std::vector<uint64_t> times_fmap_invalid(INVCOUNT);
+     for (int i = 0; i < TCOUNT; i++) {
+        std::string symb = elements[valid_tests[i]].first.s;
+        smallstring sm(symb);
+        uint64_t t1 = ticks();
+        int res = fmap.find(sm);
+        uint64_t t2 = ticks();
+        times_fmap_valid[i] = t2 - t1;
+    }
+    for (int i = 0; i < INVCOUNT; i++) {
+        std::string symb = bad_keys[invalid_tests[i]].s;
+        smallstring sm(symb);
+        uint64_t t1 = ticks();
+        int res = fmap.find(sm);
+        uint64_t t2 = ticks();
+        times_fmap_invalid[i] = t2 - t1;
+    }
+    int TOTAL = TCOUNT + INVCOUNT;
+    std::cout << "---------For Flat_Unordered_map_valid ----------" << std::endl;
+    time_it(times_fmap_valid);
+
+    
+    std::cout << "---------For Flat_Unordered_map_invalid ----------" << std::endl;
+    time_it(times_fmap_invalid);
+    
+    
+    std::unordered_map<smallstring, int, smallstringHash, smallstringEq> umap;
+    umap.max_load_factor(1.0f);
+    umap.reserve(MAXX);
+    int z = std::bit_width((unsigned long long)MAXX);
+    umap.rehash(1ll<<z);
+
+    for (auto& [k, v] : elements) {
+        umap[k] = v;
+    }
+    std::vector<uint64_t> times_umap_valid(TCOUNT);
+    std::vector<uint64_t> times_umap_invalid(INVCOUNT);
+    for (int i = 0; i < TCOUNT; i++) {
+        std::string symb = elements[valid_tests[i]].first.s;
+        smallstring sm(symb);
+        uint64_t t1 = ticks();
+        auto res = umap.find(sm);
+        uint64_t t2 = ticks();
+        times_umap_valid[i] = t2 - t1;
+    }
+    for (int i = 0; i < INVCOUNT; i++) {
+        std::string symb = bad_keys[invalid_tests[i]].s;
+        smallstring sm(symb);
+        uint64_t t1 = ticks();
+        auto res = umap.find(sm);
+        uint64_t t2 = ticks();
+        times_umap_invalid[i] = t2 - t1;
+    }
+    std::cout << "---------For Unordered_map_valid ----------" << std::endl;
+    time_it(times_umap_valid);
+    
+    std::cout << "---------For Unordered_map_invalid ----------" << std::endl;
+    time_it(times_umap_invalid);
     return 0;
 }
 
+
 int main() {
-    test1();
+    auto [elements, bad_keys] = gen_test();
+    test_correctness(elements, bad_keys);
+    test_time(elements, bad_keys);
+
     return 0;
 }
